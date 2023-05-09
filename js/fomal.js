92d16dd77edf2c3a17d28b73b14227991ea4a3aa
Fingertip-glaze-luck <Fingertip-glaze-luck@outlook.com>
2023-05-09 11:52:34 +0800

    Site updated: 2023-05-09 11:52:34

--- js/fomal.js
+++ js/fomal.js
@@ -3174,7 +3174,7 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://github.com/Fingertip-glaze-luck/tuchuang/blob/d76c75e656927fdd7daf0946d83e91ef5c61c734/imgs/bg.jpg);
+    --default-bg: url(https://cdn.staticaly.com/gh/Fingertip-glaze-luck/tuchuang@main/imgs/bg.jpg);
     --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
     --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
     --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
