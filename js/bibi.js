c263844f7b442b9fdad28a4d91192c077290dcc7
Fingertip-glaze-luck <Fingertip-glaze-luck@outlook.com>
2023-05-27 13:36:59 +0800

    Site updated: 2023-05-27 13:36:59

--- js/bibi.js
+++ js/bibi.js
@@ -3,7 +3,7 @@ let total = 0
 let nowNum = 0
 let items = []
 let page = 1
-let Url = 'https://kkapi.limerence-fxy.top/api/user/init?userName=Limerence-fxy' // 记住替换为你的API链接
+let Url = 'https://kkapi.limerence-fxy.top' // 记住替换为你的API链接
 
 
 window.addEventListener('DOMContentLoaded', () => {
